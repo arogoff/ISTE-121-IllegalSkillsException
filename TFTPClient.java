0f86260d973e78741885673c7661aa2305108b65
Garrett <66090085+Garousome@users.noreply.github.com>
2021-04-09 18:41:22 -0400

    Starting Points
    TFTPServer
    - Incorporated UDPServerThread Shell Class
    - Incorporated UDPClientThread Shell Class
    - Added GUI Components for the server (Start/Stop, Choose Folder, TextField (Directory), and Log)
    TFTPClient
    - Standard UDP Client
    - Added GUI Components for ChooseFolder Button & TextField, Upload & Download Buttons, and Log.

---
+++ TFTPClient.java
@@ -0,0 +1,123 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+/**
+ * TFTPClient - Sends 
+ * No Connect/Disconnect button - connectionless.
+ * @author  Garrett Maury, Josh R, Alex R
+ * @version 4/9/2021
+ */
+
+public class TFTPClient extends Application implements EventHandler<ActionEvent> {
+   // Window attributes
+   private Stage stage;
+   private Scene scene;
+   private VBox root;
+   
+   // These are for Row1
+   private Label lblServerIP = new Label("Server Name or IP: ");
+   private TextField tfServerIP = new TextField();
+
+   // These will be in Row2
+   private Button btnChooseFolder = new Button("Choose Folder");
+   private TextField tfSentence = new TextField();
+   
+   // Components - CENTER
+   private Button btnDownload = new Button("Download");
+   private Button btnUpload = new Button("Upload");
+   private Label lblLog = new Label("Log:");
+   private TextArea taLog = new TextArea();
+
+   // IO attributes
+   private DatagramSocket socket = null;
+   private InetAddress serverIP = null;
+   
+   // OTHER attributes
+   public static final int SERVER_PORT = 32001;
+
+   /**
+    * main program 
+    */
+   public static void main(String[] args) {
+      launch(args);
+   }
+
+   /**
+    * start - draw and set up GUI
+    */
+   public void start(Stage _stage) {
+      stage = _stage;
+      stage.setTitle("TFTPClient");
+      stage.setOnCloseRequest(
+         new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent evt) { System.exit(0); }
+         });
+      stage.setResizable(false);
+      root = new VBox(8);
+      
+      // ROW1 - FlowPane
+      FlowPane fpRow1 = new FlowPane(8,8);
+      fpRow1.setAlignment(Pos.CENTER);
+      tfServerIP.setPrefColumnCount(15);
+      fpRow1.getChildren().addAll(lblServerIP, tfServerIP);
+      root.getChildren().add(fpRow1);
+   
+      // ROW2 - Textfield for a sentence to send and Send button
+      FlowPane fpRow2 = new FlowPane(8,8);
+      fpRow2.setAlignment(Pos.CENTER);
+      fpRow2.getChildren().addAll(btnChooseFolder, tfSentence);
+      root.getChildren().add(fpRow2);
+      
+      // Buttons - Upload and Download
+      FlowPane fpbtns = new FlowPane();
+      fpbtns.setAlignment(Pos.CENTER);
+      fpbtns.getChildren().addAll(btnUpload, btnDownload);
+      root.getChildren().add(fpbtns);
+      
+      // LOG - Label + text area
+      FlowPane fpLog = new FlowPane();
+      fpLog.setAlignment(Pos.CENTER);
+      taLog.setPrefColumnCount(35);
+      taLog.setPrefRowCount(10);
+      tfSentence.setPrefColumnCount(25);
+      fpLog.getChildren().addAll(lblLog, taLog);
+      root.getChildren().add(fpLog);
+      
+      // Listen for the buttons
+      btnChooseFolder.setOnAction(this);
+   
+      scene = new Scene(root, 475, 300);
+      stage.setScene(scene);
+      stage.show();      
+      
+      // Open a DatagramSocket for IO
+      try {
+         socket = new DatagramSocket();
+      }
+      catch(SocketException se) {
+         Alert alert = new Alert(AlertType.ERROR, "Cannot create socket: " + se);
+         alert.setHeaderText("Socket Failure");
+         alert.showAndWait();
+         System.exit(1);
+      }
+   }
+
+   /** 
+    * Button dispatcher
+    */
+   public void handle(ActionEvent ae) {
+   
+   }
+
+}
---
+++ TFTPServer.java
@@ -0,0 +1,217 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * TFTPServer - Multi-Threaded UDP server with start/stop button to control
+ * server connections.
+ * @author  Garrett Maury, Josh R, Alex R
+ * @version 4/9/2021
+ */
+
+public class TFTPServer extends Application implements EventHandler<ActionEvent> {
+   // Window attributes
+   private Stage stage;
+   private Scene scene;
+   private VBox root;
+   
+   // GUI Components
+   private Label lblLog = new Label("Log:");
+   private TextArea taLog = new TextArea();
+   private Button btnStartStop = new Button("Start");
+   private Button btnChooseFolder = new Button("Choose Folder");
+   private Label lblServer = new Label("Start the server: ");
+   private TextField dir = new TextField();
+   
+   // Socket stuff
+   private ServerSocket sSocket = null;
+   public static final int SERVER_PORT = 69;
+   
+   // Other stuff
+   UDPServerThread serverThread = null;
+   
+   /**
+    * main program
+    */
+   public static void main(String[] args) {
+      launch(args);
+   }
+   
+   /**
+    * Start, draw and set up GUI
+    * Do server stuff
+    */
+   public void start(Stage _stage) {
+      // Window setup
+      stage = _stage;
+      stage.setTitle("TFTPServer");
+      stage.setOnCloseRequest(
+         new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent evt) { System.exit(0); }
+         });
+      stage.setResizable(false);
+      root = new VBox(8);
+      
+      // Start/Stop button
+      FlowPane fpStart = new FlowPane(8,8);
+      fpStart.setAlignment(Pos.BASELINE_RIGHT);
+      fpStart.getChildren().addAll(lblServer, btnStartStop);
+      btnStartStop.setOnAction(this);
+      root.getChildren().add(fpStart);
+   
+      // LOG components
+      FlowPane fpLog = new FlowPane(8,8);
+      fpLog.setAlignment(Pos.CENTER);
+      taLog.setPrefRowCount(10);
+      taLog.setPrefColumnCount(35);
+      fpLog.getChildren().addAll(lblLog, taLog, btnChooseFolder, dir);
+      root.getChildren().add(fpLog);
+      
+      // Show window
+      scene = new Scene(root, 525, 250);
+      stage.setScene(scene);
+      stage.show();      
+   }
+      
+   // Start/Stop button
+   public void handle(ActionEvent evt) {
+      String label = ((Button)evt.getSource()).getText();
+      switch(label) {
+         case "Start":
+            doStart();
+            break;
+         case "Stop":
+            doStop();
+            break;
+      }
+   } 
+   
+   public void doStart() {
+      UDPServerThread t1 = new UDPServerThread();
+      //serverThread.start();
+      btnStartStop.setText("Stop");
+   }
+   
+   public void doStop() {
+      //UDPserverThread.stopServer();
+      btnStartStop.setText("Start");
+   }
+   
+   class UDPServerThread extends Thread {
+      public void run() {
+         // Server stuff ... wait for a connection and process it
+         // Server stuff ... wait for a packet and process it
+         //try {
+            // sSocket = new ServerSocket(SERVER_PORT);
+            // UDPServerThread is an inner class, inside the main
+            // GUI class that extends Application.
+            // mainSocket is a DatagramSocket declared in the global scope
+            // and initialized to null
+            //mainSocket = new DatagramSocket(SERVER_PORT); // Binds the socket to the port
+         //}
+         /*catch(IOException ioe) {
+            log("IO Exception (1): " + ioe + "\n");
+            return;
+         }*/
+      
+         // wait for a packet from a new client, then
+         // start a client thread
+         while (true) {
+            // Socket for the client
+            // Socket cSocket = null;
+            // The socket for the client is created in the client thread
+            //byte[] holder = new byte[MAX_PACKET];
+            // packet for 1st packet from a client
+            //DatagramPacket pkt = new DatagramPacket(holder, MAX_PACKET);
+            //try {
+               // Wait for a connection and set up IO
+               // cSocket = sSocket.accept();
+               // We get a DatagramPacket, instead of a Socket, in the UDP case
+               //pkt = mainSocket.receive(); // Wait for 1st packet
+            //}
+            /*catch(IOException ioe) {
+               // Happens when mainSocket is closed while waiting
+               // to receive - This is how we stop the server.
+               return;
+            }*/
+            // Create a thread for the client
+            //UDPClientThread ct = new UDPClientThread(cSocket);
+            // Instead of passing a Socket to the client thread, we pass the 1st packet
+            //UDPClientThread ct = new UDPClientThread(pkt);
+            //ct.start();
+         
+         } // of while loop
+      } // of run   
+   }  
+
+
+   class UDPClientThread extends Thread {
+      // Since attributes are per-object items, each ClientThread has its OWN
+      // socket, unique to that client
+      private Socket cSocket;
+      //private DatagramSocket cSocket = null;
+      private DatagramPacket firstPkt = null;
+   
+      // Constructor for ClientThread
+      public UDPClientThread(DatagramPacket _pkt) {
+         //cSocket = _cSocket;
+         firstPkt = _pkt;
+         // So - the new DatagramSocket is on a DIFFERENT port,
+         // chosen by the OS. If we use cSocket from now on, then
+         // port switching has been achieved.
+         //cSocket = new DatagramSocket();
+      }
+   
+      // main program for a ClientThread
+      public void run() {
+         // log("Client connected!\n");
+         log("Client packet received!\n");
+      
+         //try {
+            // In this try-catch run the protocol, using firstPkt as
+            // the first packet in the conversation
+         //}
+         /*catch(IOException ioe) {
+            log("IO Exception (3): " + ioe + "\n");
+            // For TFTP, probably send an ERROR packet here
+            return;
+         }*/
+      
+         // As the conversation progresses, to receive a packet:
+         //byte[] holder = new holder[MAX_PACKET];
+         //DatagramPacket incoming = new DatagramPacket(holder, MAX_PACKET);
+         //cSocket.receive(incoming);
+         // Then - dissect the incoming packet and process it
+         // THE NEXT SET OF NOTES DISCUSSES HOW TO DISSECT PACKETS
+      
+         // To send a packet:
+         // Compute the contents of the outgoing packet
+         // Build the packet ... producing a DatagramPacket, outgoing
+         //cSocket.send(outgoing);
+         // THE NEXT SET OF NOTES ALSO DISCUSSES HOW TO BUILD PACKETS
+      
+         // log("Client disconnected!\n");
+         log("Client completed!\n");
+      }
+   } // End of inner class
+
+   // utility method "log" to log a message in a thread safe manner
+   private void log(String message) {
+      Platform.runLater(
+         new Runnable() {
+            public void run() {
+               taLog.appendText(message);
+            }
+         });
+   } // of log
+}
