ceb9dc7eeaa31351e9c346c10a6a0a6ce9c9e68e
Garrett <66090085+Garousome@users.noreply.github.com>
2021-05-03 15:02:19 -0400

    final client

--- TFTPClient.java
+++ TFTPClient.java
@@ -445,17 +445,6 @@ public class TFTPClient extends Application implements EventHandler<ActionEvent>
                            + "  [" + size + "]" + data[size] + "\n");
                      
                   socket.send(secondPkt.build()); //send the second packet
-                  
-                  Long elapsedTime = System.nanoTime() - startTime;
-                  Long allTimeForUploading = (elapsedTime * fileTo.length() / totalSize);
-                  Long remainingTime = allTimeForUploading - elapsedTime;
-                  
-                  double remainingTimeInSeconds = (double) remainingTime / 1_000_000_000;
-                  int seconds = (int)remainingTimeInSeconds % 60;
-                  int hour = seconds / 60;
-                  int minutes = hour % 60;
-                  hour = hour / 60;
-                  System.out.println("Time Remaining: " + hour + "hr(s) : " + String.format("%02d", minutes) + "min(s) : " + String.format("%02d", seconds) + "sec");
                      
                   if(size < 511) {
                      continueLoop = false; //if the size is less than 511, end the loop
